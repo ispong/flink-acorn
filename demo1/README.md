4f033efd903366eeccda63dfcf3dd8548f04984b
ispong <ispong@outlook.com>
2021-11-18 17:58:26 +0800

    :sparkles: up demo1

--- demo1/README.md
+++ demo1/README.md
@@ -1,2 +1,25 @@
 > demo1
 > kafka到flink  然后flink再推出到另一个kafka  
+
+```shell
+cd demo1
+mvn clean package
+cd target
+flink run demo1-0.0.1.jar
+```
+
+```shell
+# 创建管道
+sudo ./kafka-topics.sh --create --zookeeper 101.132.135.228:30099 --replication-factor 1 --partitions 1 --topic ispong-input-1
+# 检查管道是否存在
+sudo ./kafka-topics.sh --list --zookeeper 101.132.135.228:30099
+# 先检查管道是否正常传输
+sudo ./kafka-console-producer.sh --broker-list 101.132.135.228:30098 --topic ispong-input-1
+sudo ./kafka-console-consumer.sh --bootstrap-server 101.132.135.228:30098 --topic ispong-input-2 --from-beginning
+```
+
+```shell
+# 输入管道1 ispong-kafka-input1
+# 输入管道2 ispong-kafka-input2
+# 检查输出管道 ispong-kafka-input2是否有数据
+```
--- demo1/src/main/java/com/isxcode/demo1/Demo.java
+++ demo1/src/main/java/com/isxcode/demo1/Demo.java
@@ -2,12 +2,8 @@ package com.isxcode.demo1;
 
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
-import org.apache.flink.table.api.SqlDialect;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
-import org.apache.flink.table.catalog.hive.HiveCatalog;
-
-import static org.apache.flink.table.api.Expressions.$;
 
 public class Demo {
     public static void main(String[] args) {
@@ -16,43 +12,37 @@ public class Demo {
         bsEnv.enableCheckpointing(5000);
         EnvironmentSettings settings = EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build();
         StreamTableEnvironment tEnv = StreamTableEnvironment.create(bsEnv,settings);
-        tEnv.getConfig().getConfiguration().setString("pipeline.name", "isxcode-pipeline");
+        tEnv.getConfig().getConfiguration().setString("pipeline.name", "isxcode-flink-pipeline");
 
-        // --- kafka ---
-        tEnv.getConfig().setSqlDialect(SqlDialect.DEFAULT);
+        // --- from kafka ---
         tEnv.executeSql("CREATE TABLE from_kafka (\n" +
-                "   `sql` STRING," +
-                "   `select` INT" +
+                "   username STRING," +
+                "   age INT" +
                 ") WITH (\n" +
                 "   'connector'='kafka'," +
-                "   'topic'='ispong_kafka'," +
-                "   'properties.zookeeper.connect'='39.103.230.188:30121'," +
-                "   'properties.bootstrap.servers'='39.103.230.188:30120'," +
-                "   'format' = 'json'," +
-                "   'json.ignore-parse-errors' = 'false'," +
-                "   'json.fail-on-missing-field' = 'true'"+
+                "   'topic'='ispong-input-1'," +
+                "   'properties.zookeeper.connect'='101.132.135.228:30099'," +
+                "   'properties.bootstrap.servers'='101.132.135.228:30098'," +
+                "   'format' = 'csv'" +
                 ")");
 
-        // --- insert ---
+        // --- to kafka ---
+        tEnv.executeSql("CREATE TABLE to_kafka (\n" +
+                "   username STRING," +
+                "   age INT" +
+                ") WITH (\n" +
+                "   'connector'='kafka'," +
+                "   'topic'='ispong-input-2'," +
+                "   'properties.zookeeper.connect'='101.132.135.228:30099'," +
+                "   'properties.bootstrap.servers'='101.132.135.228:30098'," +
+                "   'format' = 'csv'" +
+                ")");
+
+        // from data
         Table fromData = tEnv.from("from_kafka");
-        fromData = fromData.select($("sql").as("sql"), $("select").as("select"));
 
-        // --- hive ---
-        String name = "to_hive";
-        String defaultDatabase = "cdh_dev";
-        String hiveConfDir = "/opt/flink/conf";
-
-        tEnv.getConfig().setSqlDialect(SqlDialect.HIVE);
-        HiveCatalog hive = new HiveCatalog(name, defaultDatabase, hiveConfDir);
-        tEnv.registerCatalog(name, hive);
-        tEnv.useCatalog(name);
-
-        tEnv.createTemporaryView("from_kafka_tmp", fromData);
-//        tEnv.executeSql("insert into ispong_table (username,age) values('zhangsan',4)");
-
-        tEnv.getConfig().setSqlDialect(SqlDialect.DEFAULT);
-        tEnv.executeSql("INSERT INTO ispong_table SELECT `sql`,`select` FROM from_kafka_tmp");
-//        tEnv.executeSql("select * from to_hive.cdh_dev.ispong_table").print();
+        // to data
+        fromData.executeInsert("to_kafka");
     }
 
 }
