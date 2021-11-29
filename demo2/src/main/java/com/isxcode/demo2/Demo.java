bca34179ea14e9136cbec638a51a55e4f4bac338
ispong <ispong@outlook.com>
2021-11-29 17:23:53 +0800

    :sparkles: 上传demo2

--- demo2/src/main/java/com/isxcode/demo2/Demo.java
+++ demo2/src/main/java/com/isxcode/demo2/Demo.java
@@ -16,7 +16,8 @@ public class Demo {
 
         tEnv.executeSql("CREATE TABLE from_kafka(\n" +
                 "   username STRING," +
-                "   age INT"+
+                "   age INT,"+
+                "   lucky_date TIMESTAMP(3) METADATA FROM 'timestamp'"+
                 ") WITH (\n" +
                 "   'connector'='kafka'," +
                 "   'topic'='ispong_kafka'," +
@@ -28,7 +29,8 @@ public class Demo {
 
         tEnv.executeSql("CREATE TABLE to_mysql (\n" +
                 "   username varchar(150)," +
-                "   age int"+
+                "   age int,"+
+                "   lucky_date date"+
                 ") WITH (\n" +
                 "   'connector'='jdbc'," +
                 "   'url'='jdbc:mysql://47.103.203.73:3306/VATtest'," +
@@ -40,7 +42,11 @@ public class Demo {
 
         Table fromData = tEnv.from("from_kafka");
 
-        fromData = fromData.select($("username").as("username"), $("age").as("age"));
+        // select中的字段需要根据kafka的字段顺序排序
+        fromData = fromData.select(
+                $("username").as("username"),
+                $("age").as("age"),
+                $("lucky_date").as("lucky_date"));
 
         fromData.executeInsert("to_mysql");
     }
