f622db434d39f23fd4912b11f676b99f30d5bb19
ispong <ispong@outlook.com>
2021-12-06 20:48:45 +0800

    :sparkles: 暂不做https请求

--- acorn-common/src/main/java/com/isxcode/acorn/common/constant/UrlConstants.java
+++ acorn-common/src/main/java/com/isxcode/acorn/common/constant/UrlConstants.java
@@ -2,5 +2,5 @@ package com.isxcode.acorn.common.constant;
 
 public interface UrlConstants {
 
-    String EXECUTE_URL = "https://%s:%s/flink-acorn/execute";
+    String EXECUTE_URL = "http://%s:%s/flink-acorn/execute";
 }
