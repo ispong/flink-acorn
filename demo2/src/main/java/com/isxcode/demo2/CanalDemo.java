2b1c2655e82be131083830fde31ad6564f3fe9aa
ispong <ispong@outlook.com>
2021-12-13 19:12:11 +0800

    :sparkles: 添加kafka upsert测试

--- demo2/src/main/java/com/isxcode/demo2/CanalDemo.java
+++ demo2/src/main/java/com/isxcode/demo2/CanalDemo.java
@@ -29,34 +29,34 @@ public class CanalDemo {
                 "   'canal-json.ignore-parse-errors'='true'" +
                 ")");
 
-        // 装成csv
-//        tEnv.executeSql("CREATE TABLE from_csv_kafka(\n" +
-//                "   username STRING PRIMARY KEY," +
-//                "   age INT" +
-//                ") WITH (\n" +
-//                "   'connector'='upsert-kafka'," +
-//                "   'topic'='ispong_kafka_json'," +
-//                "   'properties.group.id'='test-consumer-group'," +
-//                "   'properties.zookeeper.connect'='172.26.34.166:30099'," +
-//                "   'properties.bootstrap.servers'='172.26.34.166:30098'," +
-//                "   'key.format' = 'json'," +
-//                "   'key.json.ignore-parse-errors'='false'," +
-//                "   'value.format' = 'json'," +
-//                "   'value.json.ignore-parse-errors'='false'" +
-//                ")");
-
-//        // 输入到mysql
-        tEnv.executeSql("CREATE TABLE to_mysql (\n" +
+        // from kafka
+        tEnv.executeSql("CREATE TABLE from_kafka(\n" +
                 "   username STRING PRIMARY KEY," +
                 "   age INT" +
                 ") WITH (\n" +
-                "   'connector'='jdbc'," +
-                "   'url'='jdbc:mysql://172.26.34.166:30010/dehoop'," +
-                "   'table-name'='ispong_table_bak'," +
-                "   'driver'='com.mysql.cj.jdbc.Driver'," +
-                "   'username'='root'," +
-                "   'password'='dehoop2021'" +
+                "   'connector'='upsert-kafka'," +
+                "   'topic'='ispong_kafka_json'," +
+                "   'properties.group.id'='test-consumer-group'," +
+                "   'properties.zookeeper.connect'='172.26.34.166:30099'," +
+                "   'properties.bootstrap.servers'='172.26.34.166:30098'," +
+                "   'key.format' = 'csv'," +
+                "   'key.csv.ignore-parse-errors'='false'," +
+                "   'value.format' = 'csv'," +
+                "   'value.csv.ignore-parse-errors'='false'" +
                 ")");
+
+//        // 输入到mysql
+//        tEnv.executeSql("CREATE TABLE to_mysql (\n" +
+//                "   username STRING PRIMARY KEY," +
+//                "   age INT" +
+//                ") WITH (\n" +
+//                "   'connector'='jdbc'," +
+//                "   'url'='jdbc:mysql://172.26.34.166:30010/dehoop'," +
+//                "   'table-name'='ispong_table_bak'," +
+//                "   'driver'='com.mysql.cj.jdbc.Driver'," +
+//                "   'username'='root'," +
+//                "   'password'='dehoop2021'" +
+//                ")");
 
         // 存入json
         Table fromData = tEnv.from("from_canal_kafka");
@@ -64,7 +64,7 @@ public class CanalDemo {
                 $("username").as("username"),
                 $("age").as("age")
         );
-        fromData.executeInsert("to_mysql");
+        fromData.executeInsert("from_kafka");
 
 //        // json存入mysql
 //        Table from_csv_kafka = tEnv.from("from_csv_kafka");
