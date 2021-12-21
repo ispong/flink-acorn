701db9f7619e50316438d3403f32f03d1768fdca
ispong <ispong@outlook.com>
2021-12-21 15:08:00 +0800

    :sparkles: 保存临时作业文件夹

--- acorn-common/src/main/java/com/isxcode/acorn/common/template/AcornTemplate.java
+++ acorn-common/src/main/java/com/isxcode/acorn/common/template/AcornTemplate.java
@@ -4,6 +4,7 @@ import com.isxcode.acorn.common.constant.SecurityConstants;
 import com.isxcode.acorn.common.constant.UrlConstants;
 import com.isxcode.acorn.common.pojo.dto.AcornResponse;
 import com.isxcode.acorn.common.pojo.model.AcornModel;
+import com.isxcode.acorn.common.pojo.model.AcornModel1;
 import com.isxcode.acorn.common.properties.AcornNodeProperties;
 import com.isxcode.oxygen.core.http.HttpUtils;
 import org.springframework.stereotype.Service;
@@ -50,13 +51,13 @@ public class AcornTemplate {
             this.acornNodeProperties = acornNodeProperties;
         }
 
-        public AcornResponse execute(AcornModel acornRequest) {
+        public AcornResponse execute(AcornModel1 acornModel1) {
 
             try {
                 String executeUrl = String.format(UrlConstants.EXECUTE_URL, acornNodeProperties.getHost(), acornNodeProperties.getPort());
                 Map<String, String> headers = new HashMap<>();
                 headers.put(SecurityConstants.HEADER_AUTHORIZATION, acornNodeProperties.getKey());
-                return HttpUtils.doPost(executeUrl, headers, acornRequest, AcornResponse.class);
+                return HttpUtils.doPost(executeUrl, headers, acornModel1, AcornResponse.class);
             } catch (IOException e) {
                 e.printStackTrace();
                 return null;
