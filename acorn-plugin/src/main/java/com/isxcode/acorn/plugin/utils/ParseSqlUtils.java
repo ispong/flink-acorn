9b837196d7e3a7d1de5a43029d3cf35494bf4053
ispong <ispong@outlook.com>
2021-12-22 18:06:47 +0800

    :bug: sql解析非空校验

--- acorn-plugin/src/main/java/com/isxcode/acorn/plugin/utils/ParseSqlUtils.java
+++ acorn-plugin/src/main/java/com/isxcode/acorn/plugin/utils/ParseSqlUtils.java
@@ -6,6 +6,9 @@ public class ParseSqlUtils {
 
     public static String getTableName(String sql) {
 
+        if (sql == null) {
+            return null;
+        }
         sql = Pattern.compile("CREATE * TABLE").matcher(sql).replaceAll("");
         return Pattern.compile("\\( .*").matcher(sql).replaceAll("").trim();
     }
