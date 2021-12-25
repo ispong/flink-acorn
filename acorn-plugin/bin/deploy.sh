98d6e4884400cde1c6bdc0ebfea1581bcc6408ad
ispong <ispong@outlook.com>
2021-12-25 16:39:34 +0800

    :sparkles: 优化启动方式

---
+++ acorn-plugin/bin/deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# 获取环境变量
+ENV=prod
+for key in "$@"
+do
+    ENV=${key#*=}
+done
+
+# 关闭原有进程
+for metaResult in $(ps -e -o pid,command | grep acorn-plugin.jar)
+do
+    if [ "$metaResult" == java ]; then
+      ACORN_PID=${PRE_VAL}
+    	break
+	fi
+	PRE_VAL=${metaResult}
+done
+if [ _"${ACORN_PID}" != _ ];then
+  kill -9 "${ACORN_PID}"
+fi
+
+# 如果日志文件不存在则创建
+ACORN_LOG_DIR=~/acorn-plugin/
+if [ ! -d "$ACORN_LOG_DIR" ]; then
+    mkdir -p "$ACORN_LOG_DIR"
+fi
+ACORN_LOG=~/acorn-plugin/acorn-plugin.log
+if [ ! -f "$ACORN_LOG" ]; then
+    touch "$ACORN_LOG"
+fi
+
+# 启动项目
+ACORN_APP=../target/acorn-plugin.jar
+nohup java -jar -Xmx2048m "${ACORN_APP}" --spring.profiles.active="${ENV}" >> ${ACORN_LOG} 2>&1 &
+echo "部署成功"
