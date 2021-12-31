8920600d573e02cbd456134f52bdfa5caed84c4e
ispong <ispong@outlook.com>
2021-12-31 17:15:17 +0800

    :bug: 放开测试代码

--- acorn-plugin/src/main/java/com/isxcode/acorn/plugin/service/AcornBizService.java
+++ acorn-plugin/src/main/java/com/isxcode/acorn/plugin/service/AcornBizService.java
@@ -43,69 +43,67 @@ public class AcornBizService {
 
     public AcornData execute(AcornModel1 acornModel) {
 
-//        // 关键字检测
-//        if (acornModel.getExecuteId() == null) {
-//            throw new AcornException(AcornExceptionEnum.REQUEST_VALUE_EMPTY);
-//        }
-//
-//        // 如果需要支持hive则需要默认添加配置路径
-//        if (TemplateType.KAFKA_INPUT_HIVE_OUTPUT.equals(acornModel.getTemplateName())) {
-//            acornModel.setHiveConfigPath(acornPluginProperties.getHiveConfPath());
-//        }
-//
-//        // 解析connectorSql中表名
-//        acornModel.setFromTableName(ParseSqlUtils.getTableName(acornModel.getFromConnectorSql()));
-//        acornModel.setToTableName(ParseSqlUtils.getTableName(acornModel.getToConnectorSql()));
-//
-//        // 生成FlinkJob.java代码
-//        String flinkJobJavaCode;
-//        try {
-//            flinkJobJavaCode = FreemarkerUtils.templateToString(acornModel.getTemplateName().getTemplateFileName(), acornModel);
-//            log.debug("AcornBizService.execute.flinkJobJavaCode:" + flinkJobJavaCode);
-//        } catch (OxygenException e) {
-//            throw new AcornException(AcornExceptionEnum.JAVA_CODE_GENERATE_ERROR);
-//        }
-//
-//        // 项目临时文件路径
-//        String tmpPath = acornPluginProperties.getTmpDir() + File.separator + acornModel.getExecuteId();
-//
-//        // 创建FlinkJob.java文件
-//        String flinkJobPath = tmpPath + FileConstants.JOB_TMP_PATH + File.separator + FileConstants.JOB_FILE_NAME;
-//        FileUtils.StringToFile(flinkJobJavaCode, flinkJobPath, StandardOpenOption.WRITE);
-//
-//        // 创建pom.xml文件
-//        String flinkPomFilePath = acornPluginProperties.getTmpDir() + File.separator + acornModel.getExecuteId() + File.separator + FileConstants.POM_XML;
-//        FileUtils.copyResourceFile(FileConstants.POM_TEMPLATE_PATH, flinkPomFilePath, StandardOpenOption.WRITE);
-//
-//        // 创建日志文件
-//        String logPath = acornPluginProperties.getLogDir() + File.separator + acornModel.getExecuteId() + FileConstants.LOG_SUFFIX;
-//        FileUtils.generateFile(logPath);
-//
-//        // 执行编译且运行作业
-//        String targetFilePath = tmpPath + File.separator + "target" + File.separator + FileConstants.FLINK_JAR_NAME;
-//        String executeCommand = "mvn clean package -f " + flinkPomFilePath + " && " + "flink run " + targetFilePath;
-//        try {
-//            CommandUtils.executeCommand(executeCommand, logPath);
-//        } catch (Exception e) {
-//            throw new AcornException(AcornExceptionEnum.BUILD_COMMAND_ERROR);
-//        }
-//
-//        // 删除临时项目文件
-//        if (!acornPluginProperties.isStorageTmp()) {
-//            FileUtils.RecursionDeleteFile(Paths.get(tmpPath));
-//        }
-//
-//        // 读取日志最后一行 Job has been submitted with JobID 133d87e09f586e72e1f1fe2575d1a3c4
-//        String backlog = CommandUtils.executeBackCommand("sed -n '$p' " + logPath);
-//        if (backlog.contains(FileConstants.SUCCESS_FLINK_LOG)) {
-//            String jobId = backlog.replaceAll(FileConstants.SUCCESS_FLINK_LOG, "").trim();
-//            log.debug("AcornBizService.execute.jobId:" + jobId);
-//            return AcornData.builder().jobId(jobId).build();
-//        } else {
-//            throw new AcornException(AcornExceptionEnum.EXECUTE_COMMAND_ERROR);
-//        }
+        // 关键字检测
+        if (acornModel.getExecuteId() == null) {
+            throw new AcornException(AcornExceptionEnum.REQUEST_VALUE_EMPTY);
+        }
 
-        return AcornData.builder().jobId("123").build();
+        // 如果需要支持hive则需要默认添加配置路径
+        if (TemplateType.KAFKA_INPUT_HIVE_OUTPUT.equals(acornModel.getTemplateName())) {
+            acornModel.setHiveConfigPath(acornPluginProperties.getHiveConfPath());
+        }
+
+        // 解析connectorSql中表名
+        acornModel.setFromTableName(ParseSqlUtils.getTableName(acornModel.getFromConnectorSql()));
+        acornModel.setToTableName(ParseSqlUtils.getTableName(acornModel.getToConnectorSql()));
+
+        // 生成FlinkJob.java代码
+        String flinkJobJavaCode;
+        try {
+            flinkJobJavaCode = FreemarkerUtils.templateToString(acornModel.getTemplateName().getTemplateFileName(), acornModel);
+            log.debug("AcornBizService.execute.flinkJobJavaCode:" + flinkJobJavaCode);
+        } catch (OxygenException e) {
+            throw new AcornException(AcornExceptionEnum.JAVA_CODE_GENERATE_ERROR);
+        }
+
+        // 项目临时文件路径
+        String tmpPath = acornPluginProperties.getTmpDir() + File.separator + acornModel.getExecuteId();
+
+        // 创建FlinkJob.java文件
+        String flinkJobPath = tmpPath + FileConstants.JOB_TMP_PATH + File.separator + FileConstants.JOB_FILE_NAME;
+        FileUtils.StringToFile(flinkJobJavaCode, flinkJobPath, StandardOpenOption.WRITE);
+
+        // 创建pom.xml文件
+        String flinkPomFilePath = acornPluginProperties.getTmpDir() + File.separator + acornModel.getExecuteId() + File.separator + FileConstants.POM_XML;
+        FileUtils.copyResourceFile(FileConstants.POM_TEMPLATE_PATH, flinkPomFilePath, StandardOpenOption.WRITE);
+
+        // 创建日志文件
+        String logPath = acornPluginProperties.getLogDir() + File.separator + acornModel.getExecuteId() + FileConstants.LOG_SUFFIX;
+        FileUtils.generateFile(logPath);
+
+        // 执行编译且运行作业
+        String targetFilePath = tmpPath + File.separator + "target" + File.separator + FileConstants.FLINK_JAR_NAME;
+        String executeCommand = "mvn clean package -f " + flinkPomFilePath + " && " + "flink run " + targetFilePath;
+        try {
+            CommandUtils.executeCommand(executeCommand, logPath);
+        } catch (Exception e) {
+            throw new AcornException(AcornExceptionEnum.BUILD_COMMAND_ERROR);
+        }
+
+        // 删除临时项目文件
+        if (!acornPluginProperties.isStorageTmp()) {
+            FileUtils.RecursionDeleteFile(Paths.get(tmpPath));
+        }
+
+        // 读取日志最后一行 Job has been submitted with JobID 133d87e09f586e72e1f1fe2575d1a3c4
+        String backlog = CommandUtils.executeBackCommand("sed -n '$p' " + logPath);
+        if (backlog.contains(FileConstants.SUCCESS_FLINK_LOG)) {
+            String jobId = backlog.replaceAll(FileConstants.SUCCESS_FLINK_LOG, "").trim();
+            log.debug("AcornBizService.execute.jobId:" + jobId);
+            return AcornData.builder().jobId(jobId).build();
+        } else {
+            throw new AcornException(AcornExceptionEnum.EXECUTE_COMMAND_ERROR);
+        }
     }
 
     public AcornData getJobLog(AcornModel1 acornModel1) {
