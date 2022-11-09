c0d2368251ea94f71823c5fce06a39955c432c03
ispong <ispong@outlook.com>
2022-11-09 16:58:16 +0800

    :boom: 重构flink-acorn

--- acorn-plugins/acorn-sql-plugin/src/main/java/com/isxcode/acorn/plugin/sql/SqlJob.java
+++ acorn-plugins/acorn-sql-plugin/src/main/java/com/isxcode/acorn/plugin/sql/SqlJob.java
@@ -3,6 +3,8 @@ package com.isxcode.acorn.plugin.sql;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.TableEnvironment;
 
+import java.util.Arrays;
+
 public class SqlJob {
 
     public static void main(String[] args) {
@@ -10,6 +12,8 @@ public class SqlJob {
         EnvironmentSettings settings = EnvironmentSettings.newInstance().build();
         TableEnvironment tEnv = TableEnvironment.create(settings);
 
-        tEnv.executeSql(args[0]);
+        String sql = args[0];
+        String[] metaSql = sql.split(";");
+        Arrays.stream(metaSql).forEach(tEnv::executeSql);
     }
 }
