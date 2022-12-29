7b56471b17f548264ee44e1744cbc43378e27521
ispong <ispong@outlook.com>
2022-12-29 16:46:22 +0800

    :sparkles: 打印日志

--- acorn-plugins/acorn-sql-plugin/src/main/java/com/isxcode/acorn/plugin/sql/SqlJob.java
+++ acorn-plugins/acorn-sql-plugin/src/main/java/com/isxcode/acorn/plugin/sql/SqlJob.java
@@ -9,11 +9,21 @@ public class SqlJob {
 
     public static void main(String[] args) {
 
+        System.out.println("开始执行作业");
+
         EnvironmentSettings settings = EnvironmentSettings.newInstance().build();
+        System.out.println("构建环境成功");
+
         TableEnvironment tEnv = TableEnvironment.create(settings);
+        System.out.println("创建tEnv成功");
 
         String sql = args[0];
-        String[] metaSql = sql.split(";");
-        Arrays.stream(metaSql).forEach(tEnv::executeSql);
+        String[] sqls = sql.split(";");
+        Arrays.stream(sqls).forEach(metaSql -> {
+            System.out.println("执行sql:" + metaSql);
+            tEnv.executeSql(metaSql);
+        });
+
+        System.out.println("任务部署成功");
     }
 }
