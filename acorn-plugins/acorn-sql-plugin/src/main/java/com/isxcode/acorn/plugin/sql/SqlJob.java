4670eda047aa73b5586cdef5258cd4470d86629e
ispong <ispong@outlook.com>
2022-12-29 16:52:33 +0800

    :sparkles: 打印日志

--- acorn-plugins/acorn-sql-plugin/src/main/java/com/isxcode/acorn/plugin/sql/SqlJob.java
+++ acorn-plugins/acorn-sql-plugin/src/main/java/com/isxcode/acorn/plugin/sql/SqlJob.java
@@ -2,28 +2,32 @@ package com.isxcode.acorn.plugin.sql;
 
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.TableEnvironment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 
 public class SqlJob {
 
+    private static final Logger LOG = LoggerFactory.getLogger(SqlJob.class);
+
     public static void main(String[] args) {
 
-        System.out.println("开始执行作业");
+        LOG.info("开始执行作业");
 
         EnvironmentSettings settings = EnvironmentSettings.newInstance().build();
-        System.out.println("构建环境成功");
+        LOG.info("构建环境成功");
 
         TableEnvironment tEnv = TableEnvironment.create(settings);
-        System.out.println("创建tEnv成功");
+        LOG.info("创建tEnv成功");
 
         String sql = args[0];
         String[] sqls = sql.split(";");
         Arrays.stream(sqls).forEach(metaSql -> {
-            System.out.println("执行sql:" + metaSql);
+            LOG.info("执行sql:" + metaSql);
             tEnv.executeSql(metaSql);
         });
 
-        System.out.println("任务部署成功");
+        LOG.info("任务部署成功");
     }
 }
