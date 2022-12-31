8565080fefec5be4be80a34989d0466b1b8b0662
ispong <ispong@outlook.com>
2022-12-31 13:56:26 +0800

    :sparkles: 测试hive数据源

--- acorn-server/src/main/java/com/isxcode/acorn/server/service/AcornBizService.java
+++ acorn-server/src/main/java/com/isxcode/acorn/server/service/AcornBizService.java
@@ -145,6 +145,7 @@ public class AcornBizService {
             try {
                 PackagedProgram.Builder packagedProgramBuilder = PackagedProgram.newBuilder()
                     .setJarFile(new File(pluginJarPath))
+                    .setUserClassPaths(classpathFiles)
                     .setSavepointRestoreSettings(SavepointRestoreSettings.none());
 
                 if (!Strings.isEmpty(acornRequest.getPluginMainClass())) {
