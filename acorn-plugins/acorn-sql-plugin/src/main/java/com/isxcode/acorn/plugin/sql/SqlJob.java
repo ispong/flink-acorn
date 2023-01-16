153668fc100300e99f687197bd5c49b22f644d9a
ispong <ispong@outlook.com>
2023-01-16 15:48:04 +0800

    :sparkles: 支持数据查询 #59

--- acorn-plugins/acorn-sql-plugin/src/main/java/com/isxcode/acorn/plugin/sql/SqlJob.java
+++ acorn-plugins/acorn-sql-plugin/src/main/java/com/isxcode/acorn/plugin/sql/SqlJob.java
@@ -2,11 +2,13 @@ package com.isxcode.acorn.plugin.sql;
 
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class SqlJob {
 
@@ -17,13 +19,21 @@ public class SqlJob {
         StreamExecutionEnvironment bsEnv = StreamExecutionEnvironment.getExecutionEnvironment();
         bsEnv.enableCheckpointing(1000);
         EnvironmentSettings settings = EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build();
-        StreamTableEnvironment tEnv = StreamTableEnvironment.create(bsEnv,settings);
+        StreamTableEnvironment tEnv = StreamTableEnvironment.create(bsEnv, settings);
 
         String sql = args[0];
         String[] sqls = sql.split(";");
-        Arrays.stream(sqls).forEach(metaSql -> {
+
+        List<String> sqlList = Arrays.asList(sqls);
+
+        sqlList.forEach(metaSql -> {
             LOG.info("执行sql:" + metaSql);
-            tEnv.executeSql(metaSql);
+            if ((sqlList.indexOf(metaSql) == sqlList.size() - 1) && !metaSql.contains("insert")) {
+                Table table = tEnv.sqlQuery(metaSql);
+                table.printSchema();
+            } else {
+                tEnv.executeSql(metaSql);
+            }
         });
     }
 }
